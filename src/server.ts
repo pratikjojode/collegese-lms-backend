1e3e385f60c46712d73592e2424e8c1bb60e2a4a
pratikjojode <139045460+pratikjojode@users.noreply.github.com>
2025-09-16 23:08:39 +0530

    fix:dep

--- src/server.ts
+++ src/server.ts
@@ -39,6 +39,7 @@ const corsOptions = {
         "http://192.168.0.107:5173",
         "http://192.168.0.106:5173",
         "https://college-lms-frontend.vercel.app",
+        "https://collegese-lms-backend.onrender.com"
         
     ],
     credentials: true,
